2a78178e1eee4afa686bcc2aecadaccba9c03a06
gulshanmammadova <111847044+gulshanmammadova@users.noreply.github.com>
2022-11-13 15:26:56 +0400

    Update main.js

--- JavaScript-1/Alert msg/JS/main.js
+++ JavaScript-1/Alert msg/JS/main.js
@@ -8,15 +8,15 @@ else if(6<=time && time<=11)
   alert("Sabahiniz xeyir");
   document.body.style.backgroundColor ="#ffc500"
 }
-else if(11<time && time<=3){
+else if(11<time && time<=15){
   alert("Gunortaniz xeyir");
   document.body.style.backgroundColor ="#f7ff00";
 }
-else if(3<time && time<=8){
+else if(15<time && time<=20){
   alert("Her vaxtiniz xeyir");
   document.body.style.backgroundColor ="#b92b27";
 }
-else if(8<time && time<=23){
-  alert("axsainiz xeyir");
+else if(20<time && time<=23){
+  alert("Axsaminiz xeyir");
   document.body.style.backgroundColor ="#4286f4";
 }
